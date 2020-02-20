92022b824aa51a42588f23bd23ae4b3c35543c1f
Yuske Fukuyama <yuskefuku@gmail.com>
2020-02-21 07:58:54 +0900

    getting started

--- src/main.c
+++ src/main.c
@@ -1,4 +1,37 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+/* #include <dict.h> */
+
+#define TEXT "alice.txt"
+#define LENGTH 45
+
 int main(void)
 {
+	struct rusage before, after;
+	double time_load = 0.0, time_mimic = 0.0;
+
+	char *text = TEXT;
+	if ((FILE *file = fopen(text, "r")) == NULL)
+	{
+		printf("Could not open %s\n", text);
+		// unload();
+		return (1);
+	}
+
+	// fgetc (try mallocing every word)
+	// or read entire file and split
+
+	if (ferror(file))
+	{
+		fclose(file);
+		printf("Error reading %s\n", text);
+		return (1);
+	}
+	fclose(file);
+	// unload()
+
 	return (0);
 }
